ef77b7dca39bb1d92df4d6fd4b9099ed2f940f74
Amr Khamis <101368138+AmrKhamis1@users.noreply.github.com>
2025-03-30 15:34:39 +0200

    effects

--- src/Effects.jsx
+++ src/Effects.jsx
@@ -1,22 +1,17 @@
-import {
-  EffectComposer,
-  Bloom,
-  Vignette,
-  Noise,
-} from "@react-three/postprocessing";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 export default function Effects() {
   return (
     <>
       <EffectComposer disableNormalPass>
+        {/* Bloom Effect */}
         <Bloom
           mipmapBlur
           radius={0.5}
           threshold={0.1}
           luminanceSmoothing={0.0}
           intensity={0.06}
-        ></Bloom>
-        {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+        />
       </EffectComposer>
     </>
   );
