9e79c82142a1394a0e142a3249190882ca62e890
Amr Khamis <101368138+AmrKhamis1@users.noreply.github.com>
2025-05-01 12:42:46 +0300

    new uploads

---
+++ src/Screens.jsx
@@ -0,0 +1,18 @@
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+
+import { randFloat } from "three/src/math/MathUtils.js";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef, useEffect } from "react";
+
+export default function Screens() {
+  return (
+    <>
+      {/* <mesh position={[-60.45, 14.67, -38.85]}>
+        <planeGeometry args={[7.7, 5.9]}></planeGeometry>
+        <meshBasicMaterial color={"red"}></meshBasicMaterial>
+      </mesh> */}
+    </>
+  );
+}
